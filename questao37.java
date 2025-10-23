21f4b2612238894a2a419b9b4cb7ecd537ae75fb
RAFAEL LUCENA COSTA DE OLIVEIRA <rafael.oliveira06@cs.unipe.edu.br>
2025-10-23 09:12:14 -0300

    commit inicial

---
+++ questao37.java
@@ -0,0 +1,107 @@
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+    }
+}
+
+abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public Duck() {}
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+}
+
+interface FlyBehavior {
+    void fly();
+}
+
+class FlyWithWings implements FlyBehavior {
+    public void fly() {
+        System.out.println("I'm flying!!");
+    }
+}
+
+class FlyNoWay implements FlyBehavior {
+    public void fly() {
+        System.out.println("I can't fly");
+    }
+}
+
+class FlyRocketPowered implements FlyBehavior {
+    public void fly() {
+        System.out.println("I'm flying with a rocket!");
+    }
+}
+
+interface QuackBehavior {
+    void quack();
+}
+
+class Quack implements QuackBehavior {
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
+
+class MuteQuack implements QuackBehavior {
+    public void quack() {
+        System.out.println("<< Silence >>");
+    }
+}
+
+class Squeak implements QuackBehavior {
+    public void quack() {
+        System.out.println("Squeak");
+    }
+}
+
+class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    public void display() {
+        System.out.println("I'm a real Mallard duck");
+    }
+}
+
+class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    public void display() {
+        System.out.println("I'm a model duck");
+    }
+}
